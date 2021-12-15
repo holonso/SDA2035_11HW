e193bddbb82ee1c9fa316c66a6d4d8ec9b617f12
holonso <d1mo@live.com>
2021-12-15 23:03:10 +0900

    Добавьте файлы проекта.

---
+++ SDA2035_11HW/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDA2035_11HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinUr linur = new LinUr { k = 3, b = 1 }; // Экземпляр структуры
+            linur.Root(); // Вызов метода
+            Console.ReadKey();
+        }
+        struct LinUr // Структура
+        {
+            public double k; // Поля k и b вещественного типа
+            public double b;
+            public void Root() // Метод
+            {
+                Console.WriteLine("x = {0:F3}", (-b / k)); // 0=kx+b это x=(-b/k)
+            }
+        }
+    }
+}
